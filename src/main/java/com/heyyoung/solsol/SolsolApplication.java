0e76e6cc8c7aa08f742a1eac44c137a9f61e3c8f
장규민 <gmjang6349@gmail.com>
2025-08-19 15:18:04 +0900

    init

---
+++ src/main/java/com/heyyoung/solsol/SolsolApplication.java
@@ -0,0 +1,13 @@
+package com.heyyoung.solsol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolsolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolsolApplication.class, args);
+	}
+
+}
