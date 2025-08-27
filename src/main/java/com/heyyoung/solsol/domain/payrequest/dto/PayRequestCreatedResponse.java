801e223fa78cae37753f85b3d78a08bf740cb11a
hongjungin <jungini0000@gmail.com>
2025-08-27 21:59:28 +0900

    feat: user 엔티티에 FcmToken 추가

---
+++ src/main/java/com/heyyoung/solsol/domain/payrequest/dto/PayRequestCreatedResponse.java
@@ -0,0 +1,10 @@
+package com.heyyoung.solsol.domain.payrequest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PayRequestCreatedResponse {
+    private Long payRequestId;
+}
--- src/main/java/com/heyyoung/solsol/domain/user/repository/UserRepository.java
+++ src/main/java/com/heyyoung/solsol/domain/user/repository/UserRepository.java
@@ -12,7 +12,9 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    
+
+    Optional<User> findByUserId(Long userId);
+
     Optional<User> findByUserIdAndDeletedAtIsNull(String userId);
     
     boolean existsByStudentNumber(String studentNumber);
