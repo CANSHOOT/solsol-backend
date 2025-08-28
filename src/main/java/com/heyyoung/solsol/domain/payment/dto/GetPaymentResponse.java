736cd1564da69b45cec5be7c2d24a777a59b08ae
장규민 <gmjang6349@gmail.com>
2025-08-28 13:58:30 +0900

    hotfix: 결제 내역 시간까지 보이도록 LocalDateTime으로 응답

--- src/main/java/com/heyyoung/solsol/domain/payment/dto/GetPaymentResponse.java
+++ src/main/java/com/heyyoung/solsol/domain/payment/dto/GetPaymentResponse.java
@@ -2,14 +2,14 @@ package com.heyyoung.solsol.domain.payment.dto;
 
 import com.heyyoung.solsol.domain.payment.PaymentsEntity;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public record GetPaymentResponse(long paymentId, int originalAmount, int discountAmount, int finalAmount,
-                                 LocalDate date) {
+                                 LocalDateTime date) {
     public static GetPaymentResponse from(PaymentsEntity payment) {
         return new GetPaymentResponse(payment.getPaymentId(),
                 payment.getOriginalAmount().intValue(), payment.getDiscountAmount().intValue(),
-                payment.getFinalAmount().intValue(), payment.getCreatedAt().atZone(ZoneId.systemDefault()).toLocalDate());
+                payment.getFinalAmount().intValue(), payment.getCreatedAt().atZone(ZoneId.systemDefault()).toLocalDateTime());
     }
 }
